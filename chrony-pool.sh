3f739751cc886385f064b7d7e970803d388c2d3c
John Serock <1468970+serock@users.noreply.github.com>
2025-04-27 13:53:01 -0400

    Do not clean dependencies when removing packages

--- chrony-pool.sh
+++ chrony-pool.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-zypper --non-interactive remove --clean-deps chrony-pool-openSUSE
+zypper --non-interactive remove chrony-pool-openSUSE
 zypper --non-interactive addlock chrony-pool-openSUSE
 zypper --non-interactive install chrony-pool-empty
--- welcome.sh
+++ welcome.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-zypper --non-interactive remove --clean-deps opensuse-welcome
+zypper --non-interactive remove opensuse-welcome
 zypper --non-interactive addlock opensuse-welcome
